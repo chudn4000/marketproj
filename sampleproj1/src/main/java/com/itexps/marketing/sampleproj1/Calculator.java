bf233523106834c90ab4d089d808fbdf939e0312
chudn4000 <102880921+chudn4000@users.noreply.github.com>
2022-08-03 23:32:41 -0500

    first version

---
+++ sampleproj1/src/main/java/com/itexps/marketing/sampleproj1/Calculator.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.itexps.marketing.sampleproj1;
+
+/**
+ *
+ * @author helen
+ */
+public class Calculator {
+    public int add(int x, int y){
+        return x+y;
+    }
+    
+}
